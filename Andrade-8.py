a20e8cc5fb149ab96390b6cb23e2ef39e0f4f561
Gordiviris <ksandrade@email.neit.edu>
2022-03-02 22:46:57 -0500

    week8

--- Andrade-8.py
+++ Andrade-8.py
@@ -5,75 +5,31 @@
 
 
 #========== FUNCTIONS ==========
-def chart(num,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o):
+def chart(num,a,b,c):
+    print("         A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4\n")
+    for i in range (0,5):
+        print("row {0:2}".format(i+1),end="  ")
+        for j in range (0,num):
+            print(a[i][j],end=" ")
+        print()
     
-    print("     A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4\n")
-    print("1  ",end=" ")
-    for x in range (0,num):
-        print(a[x],end = " ")
-    print("")
-    print("2  ",end=" ")    
-    for x in range (0,num):
-        print(b[x],end = " ")
-    print("")  
-    print("3  ",end=" ")
-    for x in range (0,num):
-        print(c[x],end = " ")
-    print("")
-    print("4  ",end=" ")
-    for x in range (0,num):
-        print(d[x],end = " ")
-    print("")
-    print("5  ",end=" ")
-    for x in range (0,num):
-        print(e[x],end = " ")
-    print("")
-    print("6  ",end=" ")
-    for x in range (0,num):
-        print(f[x],end = " ")
-    print("")
-    print("7  ",end=" ")
-    for x in range (0,num):
-        print(g[x],end = " ")
-    print("")
-    print("8  ",end=" ")
-    for x in range (0,num):
-        print(h[x],end = " ")
-    print("")
-    print("9  ",end=" ")
-    for x in range (0,num):
-        print(i[x],end = " ")
-    print("")
-    print("10 ",end=" ")
-    for x in range (0,num):
-        print(j[x],end = " ")
-    print("")
-    print("11 ",end=" ")
-    for x in range (0,num):
-        print(k[x],end = " ")
-    print("")
-    print("12 ",end=" ")
-    for x in range (0,num):
-        print(l[x],end = " ")
-    print("")
-    print("13 ",end=" ")
-    for x in range (0,num):
-        print(m[x],end = " ")
-    print("")
-    print("14 ",end=" ")
-    for x in range (0,num):
-        print(n[x],end = " ")
-    print("")
-    print("15 ",end=" ")
-    for x in range (0,num):
-        print(o[x],end = " ")
-    print()
 
+    for i in range (0,5):
+        print("row {0:2}".format(i+6),end="  ")
+        for j in range (0,num):
+            print(b[i][j],end=" ")
+        print()
 
+    for i in range (0,5):
+        print("row {0:2}".format(i+11),end="  ")
+        for j in range (0,num):
+            print(c[i][j],end=" ")
+        print()
+    
 
-def numrow(a):
+def numrow():
     num = int(input("Enter desired row number:  "))
-    while num > a or num < 1:
+    while num > 15 or num < 1:
         print("*ERROR* Invalid Selection")
         num = int(input("Enter desired row number:  "))
 
@@ -82,8 +38,12 @@ def numrow(a):
 
 
 def numcol():
-    num = input("Enter desired seat letter: ")
-    return num
+    num = input("Enter desired seat letter: ").lower()
+    if (num == "1" or num == "2" or num == "3" or num == "4"):
+        new = ord(num) - 22
+    else:
+        new = ord(num) - 96
+    return new
 
 
 
@@ -105,21 +65,81 @@ def tixprice(num):
 
 
 
-def update(rownum,colnum,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o):
-    if rownum == 1:
-        if a[colnum] == "*":
-            taken = "true"
-        else:
-            taken = "false"
-            a[colnum] = "*"
+def update(num,rownum,colnum,a,b,c):
 
+    avail = "false"
+    while avail == "false":
+        
+        if(rownum > 0 and rownum <= 5):
+            if a[rownum-1][colnum] == "*":
+                avail = "false"
+                print("\nSeat is already taken, please make another seleection!")
+                rownum = numrow()
+                colnum = numcol()
+            else:
+                avail = "true"
+                a[rownum-1][colnum] = "*"
+
+        elif(rownum >= 6 and rownum <= 10):
+            if b[rownum-6][colnum] == "*":
+                avail = "false"
+                print("\nSeat is already taken, please make another seleection!")
+                rownum = numrow()
+                colnum = numcol()
+            else:
+                avail = "true"
+                b[rownum-6][colnum] = "*"
+
+        elif(rownum >= 11 and rownum <= 15):
+            if c[rownum-11][colnum] == "*":
+                avail = "false"
+                print("\nSeat is already taken, please make another seleection!")
+                rownum = numrow()
+                colnum = numcol()
+            else:
+                avail = "true"
+                c[rownum-11][colnum] = "*"
+
+    key = input("Would you like to see available seats? [Y/N]: ").lower()
     
+    while key == "y":
+        counter = 0
+        
+        print("         A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4\n")
+        for i in range (0,5):
+            print("row {0:2}".format(i+1),end="  ")
+            countRow = 0
+            for j in range (0,num):
+                print(a[i][j],end=" ")
+                if a[i][j] == "*":
+                    countRow += 1 
+                    counter +=1
+            print("....{} seats Available".format(30-countRow))
+        
 
-    
+        for i in range (0,5):
+            print("row {0:2}".format(i+6),end="  ")
+            countRow = 0
+            for j in range (0,num):
+                print(b[i][j],end=" ")
+                if b[i][j] == "*":
+                    countRow += 1 
+                    counter += 1
+            print("....{} seats Available".format(30-countRow))
 
-    return taken
+        for i in range (0,5):
+            print("row {0:2}".format(i+11),end="  ")
+            countRow = 0
+            for j in range (0,num):
+                print(c[i][j],end=" ")
+                if c[i][j] == "*":
+                    countRow += 1 
+                    counter += 1
+            print("....{} seats Available".format(30-countRow))
 
+        print("\nThere are {} total seats available!".format(450-counter))
 
+        key = "n"
 
 
 #========== Main Code ==========
@@ -140,6 +160,7 @@ row13 = [""]
 row14 = [""]
 row15 = [""]
 
+
 rows = 15
 seats = 31
 for x in range (0,seats):
@@ -159,19 +180,33 @@ for x in range (0,seats):
     row14.append("#")
     row15.append("#")
 
-chart(seats,row1,row2,row3,row4,row5,row6,row7,row8,row9,row10,row11,row12,row13,row14,row15)
+sec1 = [row1,row2,row3,row4,row5]
+sec2 = [row6,row7,row8,row9,row10]
+sec3 = [row11,row12,row13,row14,row15]
+#print(sec1)
+#print(sec2)
+#print(sec3)
+
+chart(seats,sec1,sec2,sec3)
 print("")
 numtix = amount()
 totalprice= 0
 for x in range (0,numtix):
     y = x+1
-    print("Ticket #{}".format(y))
-    inrow = numrow(rows)
+    print("\nTicket #{}".format(y))
+    inrow = numrow()
     incol = numcol()
+    #print(inrow)
+    #print(incol)
     price = tixprice(inrow)
     totalprice = totalprice + price
-    update(inrow,incol,row1,row2,row3,row4,row5,row6,row7,row8,row9,row10,row11,row12,row13,row14,row15)
-
-print("total {0}".format(totalprice))
+    check = "y"
+    while check == "y":
+        check = input("Would you like to see your current total? [Y/N] ").lower()
+        if check == "y":
+            print("\nYour total is ${0:.2f}".format(totalprice))
+            check = "n"
+    update(seats,inrow,incol,sec1,sec2,sec3)
     
 
+print("\nYour Grandtotal is ${0:.2f}".format(totalprice))
